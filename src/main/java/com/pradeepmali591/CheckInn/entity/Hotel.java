650af6af6c950294c4a5f9df8fd3404d39be0284
Pradeep Mali <malipradeep591@gmail.com>
2026-08-04 21:04:11 +0530

    Add Hotel entity with embedded contact information

---
+++ src/main/java/com/pradeepmali591/CheckInn/entity/Hotel.java
@@ -0,0 +1,45 @@
+package com.pradeepmali591.CheckInn.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.cglib.core.Local;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "hotel")
+public class Hotel {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(columnDefinition = "TEXT")
+    private String[] photos;
+
+    @Column(columnDefinition = "TEXT")
+    private String[] amenities;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private Boolean active;
+
+    @Embedded
+    private HotelContactInfo contactInfo;
+}
