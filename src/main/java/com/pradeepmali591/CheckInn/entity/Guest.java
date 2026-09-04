77c66bac5a20bcae310f918840226c5a159fb9e9
Pradeep Mali <malipradeep591@gmail.com>
2026-09-04 19:41:40 +0530

    Create Guest entity

---
+++ src/main/java/com/pradeepmali591/CheckInn/entity/Guest.java
@@ -0,0 +1,38 @@
+package com.pradeepmali591.CheckInn.entity;
+
+import com.pradeepmali591.CheckInn.entity.enums.Gender;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+public class Guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private Integer age;
+
+    @ManyToMany(mappedBy = "guests")
+    private Set<Booking> bookings;
+
+
+
+
+
+}
